78589cbfa4cc1954f49b17332267d225b3a008c0
Marcio Antônio de Andrade Bortoloti <mbortoloti@uesb.edu.br>
2025-09-15 21:59:41 -0300

    update code

--- EXAMPLE.jl
+++ EXAMPLE.jl
@@ -1,15 +1,9 @@
-import Manifolds as mf
-import ManifoldDiff as md
-using Manopt
-import LinearAlgebra as la
-using Printf
-using Plots
-using Random
-using BenchmarkProfiles
-using LaTeXStrings
-using DelimitedFiles
+using LinearAlgebra
+
 
 include("adap_rppm.jl")
+
+
 #include("ppmnn.jl")
 
 
@@ -17,33 +11,38 @@ n = 2; # dimension of the SPD matrices
 
 ngueses = 1;
 
-seed = MersenneTwister(1234)
+#seed = MersenneTwister(1234)
 
-    g1(_,X) = la.logdet(X)^4 / 12.0
-    grad_g1(_,X) = la.logdet(X)^3 * inv(X) / 3.0
+    g1(_,X) = logdet(X)^4 / 12.0
+    grad_g1(_,X) = logdet(X)^3 * inv(X) / 3.0
 
 
-    g2(_,X) = la.logdet(X)^2
-grad_g2(_,X) = 2.0 * la.logdet(X) * inv(X)
+    g2(_,X) = logdet(X)^2
+grad_g2(_,X) = 2.0 * logdet(X) * inv(X)
 
-h(M,X) = la.logdet(X)
-∂h(M,X) = la.inv(X)
+h(M,X) = logdet(X)
+∂h(M,X) = inv(X)
 
 
 
        
     
-    # Set Manifold
-    global M = mf.SymmetricPositiveDefinite(n)
+# Set Manifold
+M = mf.SymmetricPositiveDefinite(n)
 
-    # Set initial guess
-    #global X0 = mf.rand(seed,M)
-    global X0 = log(n)*Matrix{Float64}(la.I,n,n)
-    #maxiter = 1000
-    ϵ = 1.e-8
-    maxiter = 1000
-        λ0 = 1.e-4
-        S,error,iter,λk = adap_rppm(M,X0,g1,grad_g1,g2,grad_g2,h,∂h,λ0,maxiter,ϵ);     
-        println("End of adap_rppm")           
-        # S,error = ppmnn_1(M,g1,grad_g1,g2,grad_g2,h,grad_h,X0)
+#  Initial guess
+X0 = log(n)*Matrix{Float64}(I,n,n)
+
+# Tolerance for checking convergence
+ϵ = 1.e-8
+
+# Maximum number of iterations
+maxiter = 1000
+
+# Initial λ
+λ0 = 1.e-4
+
+S,error,iter,λk = adap_rppm(M,X0,g1,grad_g1,g2,grad_g2,h,∂h,λ0,maxiter,ϵ);     
+
+println("End of adap_rppm")           
    
--- adap_rppm.jl
+++ adap_rppm.jl
@@ -1,3 +1,8 @@
+import Manifolds as mf
+import ManifoldDiff as md
+using Manopt
+using LinearAlgebra
+
 function build_fk(M :: mf.AbstractManifold, Y :: Matrix{Float64}, Zk :: Matrix{Float64}, 
         g1 :: Function, λk :: Float64)
 
@@ -15,11 +20,6 @@ function build_grad_fk(M :: mf.AbstractManifold, Y :: Matrix{Float64}, Zk :: Mat
 
 end
 ###########################################################################################################
-#function symm(Z)
-#    return 0.5 * (Z + la.transpose(Z))
-#end
-
-###########################################################################################################
 function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Function, grad_g1 :: Function, 
         g2 :: Function, grad_g2 :: Function, h :: Function, grad_h :: Function, λ0 :: Float64,
         maxiter :: Int,ϵ :: Float64;return_state = false)
@@ -29,7 +29,7 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
     
     # Set initial guess
     Xk = copy(M,X0)
-    @printf("%5d %15.10e\n",0,f(M,Xk))
+    println(0,"  ",f(M,Xk))
     f_Xk = f(M,Xk)
   
     # Set λk
@@ -41,13 +41,9 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
     for iter in 1:maxiter
 
         # Calculate riemannian gradient of g2
-        #Vk = mf.project(M,Xk,grad_g2(M,Xk))
-        #Vk = Xk * grad_g2(M,Xk) * Xk
         Vk = md.riemannian_gradient(M,Xk,grad_g2(M,Xk))
 
         # Calculate riemannian subgradient of h
-        #Wk = mf.project(M,Xk,grad_h(M,Xk))
-        #Wk = Xk * grad_h(M,Xk) * Xk
         Wk = md.riemannian_gradient(M,Xk,grad_h(M,Xk))
         
         WkmVk = Wk-Vk 
@@ -62,7 +58,6 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
             grad_fk(M,Y) = build_grad_fk(M,Y,Zk,grad_g1,λk)
 
             try
-                #println("Xk = $Xk")
                 # Solving subproblem
                 Yk = Manopt.trust_regions(M, fk, grad_fk, Zk;
                      stepsize=ArmijoLinesearch(M;contraction_factor=0.5,initial_stepsize=1.0,
@@ -70,9 +65,7 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
                      #debug=[:Iteration, :Cost, "\n",:Stop],
                      stopping_criterion=StopAfterIteration(5000)|StopWhenChangeLess(M,1e-12)
                      );
-                #println("Yk = $Yk") 
-                #Yk = mf.exp(M,Yk,1.e-12*mf.zero_vector(M,Yk))
-            
+               
                 global dist_Xk_Yk = mf.distance(M,Xk,Yk)
 
                 f_Yk = f(M,Yk)
@@ -80,7 +73,7 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
 
              if dist_Xk_Yk < ϵ
                     
-                    @printf("%5d %20.15e %+20.15e %20.15e\n",iter,dist_Xk_Yk,f_Yk,λk)
+                    println(iter,"  ",dist_Xk_Yk,"   ",f_Yk,"   ",λk)
                     
                     if return_state
                         return cost
@@ -88,7 +81,6 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
                         return Yk,0,iter,λk
                     end
                 end
-                #f_Yk = f(M,Yk)
 
                 if ~(f_Yk - f_Xk > -0.25 * λk * dist_Xk_Yk^2)
                     Xk = Yk
@@ -96,12 +88,10 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
                     break
                 else
                     λk = 2.0 * λk
-                    #println("Updating λk because (23) is false")
                 end
              #end
           catch
             λk = 2.0 * λk
-           # println("Updating λk because (21) could not be solved")
           end
 
         end
@@ -109,7 +99,7 @@ function adap_rppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Functi
         push!(cost,f_Xk)
 
         # Print info
-        @printf("%5d %20.15e %+20.15e %20.15e\n",iter,dist_Xk_Yk,f_Xk,λk)
+        println(iter,"  ",dist_Xk_Yk,"   ",f_Xk,"   ",λk)
         
         # Update sequence
         #Xk = Yk
